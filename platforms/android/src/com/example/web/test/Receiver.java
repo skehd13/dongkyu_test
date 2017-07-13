3e5c5088f956566c69d84a913f8c62f300c2cb59
master <skehd133@naver.com>
2017-07-13 17:38:12 +0900

    log추

--- platforms/android/src/com/example/web/test/Receiver.java
+++ platforms/android/src/com/example/web/test/Receiver.java
@@ -9,7 +9,7 @@ import android.util.Log;
 import com.ibm.MFPApplication;
 
 /**
- * Created by test on 2017. 7. 13..
+ * Created by Dongkyulee on 2017. 7. 13..
  */
 
 public class Receiver extends BroadcastReceiver {
@@ -18,11 +18,13 @@ public class Receiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d("Receiver",intent.getAction());
         if (intent.getAction().equals("dongk.web.start")) {
+            Log.d("Receiver", "start Display");
             Intent intent1 = new Intent(context, MainActivity.class);
             intent1.putExtra("url", "https://display.do-order.com");
             intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             context.startActivity(intent1);
         } else if (intent.getAction().equals("dongk.web.start.google")) {
+            Log.d("Receiver", "start Google");
             Intent intent1 = new Intent(context, MainActivity.class);
             intent1.putExtra("url", "https://www.google.com");
             intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
